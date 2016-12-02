e11d75ffbcb4f6d41d3f339195ddee6d09f48d9d
wdq <1215971936@qq.com>
2016-12-02 11:02:02 +0800

    div内大图移动查看全景完整代码

--- imgView.js
+++ imgView.js
@@ -21,8 +21,8 @@
             width: 500,
             height: 400,
             cursor:'move',
-            imgwidth:1300,
-            imgheight:1900
+            imgwidth:$img.width(),
+            imgheight:$img.height()
         },settings);
 
         // CSS
@@ -33,55 +33,63 @@
 
         $img.css('position', 'absolute');
 
-        $img.css('left', settings['width'] / 2 - settings['imgwidth'] / 2);
-        $img.css('top', settings['height'] / 2 - settings['imgheight'] / 2);
+        //img compelete
+        if(settings['imgwidth'] && settings['imgheight']){
+            loaded();
+        }
 
-        // Events
-        $img.mousedown(function (event) {
-            $container.css('cursor', settings.cursor);
-            $img.data('mousedown', true);
-            settings['pageX'] = event.pageX;
-            settings['pageY'] = event.pageY;
-            return false;
-        });
+        function loaded(){
+            //img init
+            $img.css('left', settings['width'] / 2 - settings['imgwidth'] / 2);
+            $img.css('top', settings['height'] / 2 - settings['imgheight'] / 2);
 
-        $(document).mouseup(function () {
-            $container.css('cursor', 'default');
-            $img.data('mousedown', false);
-            return false;
-        });
-
-        $(document).mousemove(function (event) {
-            if ($img.data('mousedown')) {
-                var dx = event.pageX - settings['pageX'];
-                var dy = event.pageY - settings['pageY'];
-                if ((dx == 0) && (dy == 0)) {
-                    return false;
-                }
-
-                var newX = parseInt($img.css('left')) + dx;
-                if (newX > 0) newX = 0;
-                if (newX < settings['width'] - settings['imgwidth'])
-                    newX = settings['width'] - settings['imgwidth'] + 1;
-                var newY = parseInt($img.css('top')) + dy;
-                if (newY > 0) newY = 0;
-                if (newY < settings['height'] - settings['imgheight'])
-                    newY = settings['height'] - settings['imgheight'] + 1;
-
-                if (settings['width'] >= settings['imgwidth']) {
-                    newX = settings['width'] / 2 - settings['imgwidth'] / 2;
-                }
-                if (settings['height'] >= settings['imgheight']) {
-                    newY = settings['height'] / 2 - settings['imgheight'] / 2;
-                }
-
-                $img.css('left', newX + 'px');
-                $img.css('top', newY + 'px');
-
+            // Events
+            $img.mousedown(function (event) {
+                $container.css('cursor', settings.cursor);
+                $img.data('mousedown', true);
                 settings['pageX'] = event.pageX;
                 settings['pageY'] = event.pageY;
-            }
-            return false;
-        });
+                return false;
+            });
+
+            $(document).mouseup(function () {
+                $container.css('cursor', 'default');
+                $img.data('mousedown', false);
+                return false;
+            });
+
+            $(document).mousemove(function (event) {
+                if ($img.data('mousedown')) {
+                    var dx = event.pageX - settings['pageX'];
+                    var dy = event.pageY - settings['pageY'];
+                    if ((dx == 0) && (dy == 0)) {
+                        return false;
+                    }
+
+                    var newX = parseInt($img.css('left')) + dx;
+                    if (newX > 0) newX = 0;
+                    if (newX < settings['width'] - settings['imgwidth'])
+                        newX = settings['width'] - settings['imgwidth'] + 1;
+                    var newY = parseInt($img.css('top')) + dy;
+                    if (newY > 0) newY = 0;
+                    if (newY < settings['height'] - settings['imgheight'])
+                        newY = settings['height'] - settings['imgheight'] + 1;
+
+                    if (settings['width'] >= settings['imgwidth']) {
+                        newX = settings['width'] / 2 - settings['imgwidth'] / 2;
+                    }
+                    if (settings['height'] >= settings['imgheight']) {
+                        newY = settings['height'] / 2 - settings['imgheight'] / 2;
+                    }
+
+                    $img.css('left', newX + 'px');
+                    $img.css('top', newY + 'px');
+
+                    settings['pageX'] = event.pageX;
+                    settings['pageY'] = event.pageY;
+                }
+                return false;
+            });
+        }
     };
 })(jQuery);
